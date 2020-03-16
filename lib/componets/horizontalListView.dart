0e0d9988f768978ab86a1f96e00f4273ae967992
HirushaR <hirusharandunu11@gmail.com>
2020-03-16 11:52:30 +0530

    add the categoris

--- lib/componets/horizontalListView.dart
+++ lib/componets/horizontalListView.dart
@@ -8,6 +8,34 @@ class HorizontalList extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
+          Category(
+            image_location: 'images/cats/accessories.png',
+            image_caption: 'accessories',
+          ),
+          Category(
+            image_location: 'images/cats/formal.png',
+            image_caption: 'formal',
+          ),
+          Category(
+            image_location: 'images/cats/informal.png',
+            image_caption: 'informal',
+          ),
+          Category(
+            image_location: 'images/cats/jeans.png',
+            image_caption: 'jeans',
+          ),
+          Category(
+            image_location: 'images/cats/shoe.png',
+            image_caption: 'shoe',
+          ),
+          Category(
+            image_location: 'images/cats/dress.png',
+            image_caption: 'dress',
+          ),
 
         ],
       ),
@@ -25,7 +53,22 @@ class Category extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(padding: const EdgeInsets.all(2.0),
+    child: InkWell(
+      onTap: (){},
+      child: Container(
+        width: 100.0,
+        child: ListTile(
+          title: Image.asset(image_location,
+          width: 100.0,
+          height: 60.0,),
+          subtitle: Container(
+            alignment: Alignment.topCenter,
+            child: Text(image_caption,),
+          )
+        ),
+      ),
+    ),);
   }
 }
 
