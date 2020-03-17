552613809c87c9fe8f41cafc23444e0ee9491ab2
HirushaR <hirusharandunu11@gmail.com>
2020-03-17 12:03:04 +0530

    cart state

---
+++ lib/componets/cart_products.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class _cartState extends StatefulWidget {
+  @override
+  __cartStateState createState() => __cartStateState();
+}
+
+class __cartStateState extends State<_cartState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
